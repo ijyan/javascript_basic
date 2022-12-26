c3160ed219f1885c138b92119190711a265edc32
kimkyeonghye <guuhwi@gmail.com>
2022-12-26 20:58:22 +0900

    feat: data types / let, const and var

--- contents/01-Fundamentals-Part-1/lecture/script.js
+++ contents/01-Fundamentals-Part-1/lecture/script.js
@@ -1,9 +1,20 @@
+/**
+ * Values and Variables
+ */
+/*
 const js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 console.log('Jonas');
 console.log(23);
 
+const firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
 // 변수 선언(declaring a varable)
 // camelCase
 const myFirstJob = 'Programmer';
@@ -11,3 +22,38 @@ const myCurrentJob = 'Teacher';
 
 console.log(myFirstJob);
 console.log(myCurrentJob);
+*/
+
+/**
+ * Data Types
+ */
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true); // boolean
+console.log(typeof javascriptIsFun); // boolean
+console.log(typeof 23); // number
+console.log(typeof 'jonas'); // string
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun); // string
+
+let year;
+console.log(year);
+console.log(typeof year); // undefined
+
+year = 1003;
+console.log(typeof year); // number
+
+console.log(typeof null); // object
+*/
+
+/**
+ * let, const and var
+ */
+let age = 30;
+age = 31;
+
+const birthYear = 2000;
+// birthYear = 2000;
