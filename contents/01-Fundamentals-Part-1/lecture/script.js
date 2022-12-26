6848cc4175c8748a1ee405a88204f0bdc3fd3b0a
kimkyeonghye <guuhwi@gmail.com>
2022-12-26 20:00:05 +0900

    feat: Values and Variables

---
+++ contents/01-Fundamentals-Part-1/lecture/script.js
@@ -0,0 +1,13 @@
+const js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+// 변수 선언(declaring a varable)
+// camelCase
+const myFirstJob = 'Programmer';
+const myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
