79326c675d611de3544b0a3a8d0495d38b04feed
kimkyeonghye <guuhwi@gmail.com>
2022-12-28 21:10:41 +0900

    feat: practice coding challenge

--- contents/01-Fundamentals-Part-1/practice/script.js
+++ contents/01-Fundamentals-Part-1/practice/script.js
@@ -1,35 +1,42 @@
 /**
- * LECTURE: Values and Variables
- * 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
- * 2. Log their values to the console.
+ * Coding Challenge #1
  */
+// TEST DATA 1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 95;
+const heightJohn = 1.95;
 
-const country = 'Korea';
-const continent = 'Yangsan';
-let population = 10;
+// TEST DATA 2
+/*
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+*/
+const BMI = (mass, height) => mass / height ** 2;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+const BMIMark = BMI(massMark, heightMark);
+const BMIJohn = BMI(massJohn, heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(`BMIMark: ${BMIMark}
+BMIJohn: ${BMIJohn}
+markHigherBMI: ${markHigherBMI}`);
 
 /**
- * LECTURE: Data Types
- * 1. Declare a variable called 'isIsland' and set its value according to your country.
- * The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
- * 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console.
+ * Coding Challenge #2
  */
-const isIsland = false;
-let language;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
-
-/**
- * LECTURE: let, const and var
- * 1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)
- * 2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
- * 3. Try to change one of the changed variables now, and observe what happens
- */
-language = 'Korean';
-// LECTURE: Values and Variables의 country, continent 가 const 로 변경되어야 함.
+if (BMIMark > BMIJohn) {
+  console.log(
+    `Mark's BMI(${BMIMark.toFixed(
+      1,
+    )}) is higher than John's BMI(${BMIJohn.toFixed(1)})`,
+  );
+} else {
+  console.log(
+    `John's BMI(${BMIJohn.toFixed(
+      1,
+    )}) is higher than Mark's BMI(${BMIMark.toFixed(1)})`,
+  );
+}
