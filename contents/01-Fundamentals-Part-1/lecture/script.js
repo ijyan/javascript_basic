3ef88871e8f41b6b8fd6ad46af6dc65ff7de1eb2
kimkyeonghye <guuhwi@gmail.com>
2022-12-29 20:19:55 +0900

    feat: Study Type Conversion and Coercion && Trythy and Falsy Values && Equality Operators: == vs. ===

--- contents/01-Fundamentals-Part-1/lecture/script.js
+++ contents/01-Fundamentals-Part-1/lecture/script.js
@@ -162,3 +162,83 @@ if (birthYear <= 2000) {
 }
 console.log(century);
  */
+
+/**
+ * Type Conversion and Coercion
+ */
+/*
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23));
+
+// type coercion
+console.log(`I am ${23} years old`);
+console.log('23' - '10' - 3);
+console.log('10' / 2);
+
+// eslint-disable-next-line
+let n = '3' + 1; // 11
+n -= 1;
+console.log(n);
+*/
+
+/**
+ * Trythy and Falsy Values
+ */
+/*
+// 5 falsy values: 0, '', NaN, undefined, null
+console.log(Boolean(0));
+console.log(Boolean('0'));
+console.log(Boolean(''));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+
+const money = 100;
+if (money) {
+  console.log(`Don't spend it all`);
+} else {
+  console.log(`You shold get a job!`);
+}
+
+const height = 0;
+if (height) {
+  console.log(`YAY! Height is defined`);
+} else {
+  console.log(`Height is UNDEFINED`);
+}
+*/
+
+/**
+ * Equality Operators: == vs. ===
+ */
+/*
+const age = 18;
+if (age === 18) console.log('You just became an adult :D (strict)');
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favorite = +prompt(`What's your favorit number?`);
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  // 22 === 23 -> FALSE
+  console.log('Cool! 23 is an amzaing number!');
+} else if (favorite === 7) {
+  console.log('7 is also a cool number');
+} else if (favorite === 9) {
+  console.log('9 is also a cool number');
+} else {
+  console.log('Number is not 23 or 7 or 9');
+}
+
+if (favorite !== 23) console.log('Why not 23?');
+*/
