d6cd9e3dc38bbb0155627ef0a420aca0ff65861e
kimkyeonghye <guuhwi@gmail.com>
2023-04-19 21:49:06 +0900

    feat: pig-game - hold 버튼 이벤트 추가

--- project/pig-game/script.js
+++ project/pig-game/script.js
@@ -11,7 +11,7 @@ const $btnNew = document.querySelector('.btn--new');
 const $btnRoll = document.querySelector('.btn--roll');
 const $btnHold = document.querySelector('.btn--hold');
 
-const $HIDDEN = document.querySelector('.hidden');
+const $HIDDEN = 'hidden';
 
 // 초기화
 $scoreZero.textContent = 0;
@@ -22,28 +22,62 @@ $dice.classList.add($HIDDEN);
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = () => {
+  // 플레이어 변환
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  $playerZero.classList.toggle('player--active');
+  $playerOne.classList.toggle('player--active');
+};
 const rollingDice = () => {
-  // 1. 랜덤 숫자 생성
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // 1. 랜덤 숫자 생성
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. 랜덤 숫자를 화면에 표시
-  $dice.classList.remove($HIDDEN);
-  $dice.src = `dice-${dice}.png`;
+    // 2. 랜덤 숫자를 화면에 표시
+    $dice.classList.remove($HIDDEN);
+    $dice.src = `dice-${dice}.png`;
 
-  // 3. 숫자가 1이면 플레이어 변환
-  if (dice !== 1) {
-    // 현재 점수에 더함
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // 플레이어 변환
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    $playerZero.classList.toggle('player--active');
-    $playerOne.classList.toggle('player--active');
+    // 3. 숫자가 1이면 플레이어 변환
+    if (dice !== 1) {
+      // 현재 점수에 더함
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // 플레이어 변환
+      switchPlayer();
+    }
+  }
+};
+
+const clickHold = () => {
+  if (playing) {
+    // 현재 점수를 현재 플레이어의 점수에 추가
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 플레이어의 점수가 100보다 크거나 같은지 확인
+    if (scores[activePlayer] >= 10) {
+      // 맞으면 게임을 끝냄
+      playing = false;
+      $dice.classList.add($HIDDEN);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 아니면 플레이어 변환
+      switchPlayer();
+    }
   }
 };
 
 $btnRoll.addEventListener('click', rollingDice);
+$btnHold.addEventListener('click', clickHold);
